673b4d33d40dfb3b9557601790164608ef07fd81
LukeStockham <71759405+LukeStockham@users.noreply.github.com>
2020-10-22 14:15:17 +1000

    Add files via upload

--- acceleration2ndorder.m
+++ acceleration2ndorder.m
@@ -1,14 +1,26 @@
-% variables required: v (velocity) vector and h (step size), time vector
-% (t) for plotting
+function [abs_accel_max, accel_max_time] = acceleration2ndorder(v, t, h, n)
+%ACCELERATION2NDORDER Gets the max acceleration experienced by the jumper
+% [abs_accel_max, accel_max_time] = acceleration2ndorder(v, t, h, n)
+% returns the absolute max acceleration experienced by the person during
+% the bungee jump and the time in seconds at which this occurs, found from
+% numerical differentiation of the velocity array (v)
 
 accel_2ndorder = zeros(size(t));
-fyft=@(t) v(t);
+
+fyft = @(t) v(t);
+
+% Perform iterations for numerical differentiation
 for j = 2:n
-    accel_2ndorder(j) = (fyft(j+1)-fyft(j-1))/(2*h);
+    accel_2ndorder(j) = (fyft(j + 1) - fyft(j - 1))/(2*h);
 end
 
-[abs_accel_max,accel_max_t] = max(abs(accel_2ndorder));
+% Find the max acceleration and iteration at which this occurs
+[abs_accel_max, accel_max_t] = max(abs(accel_2ndorder));
 
+% Get the time in seconds for when the max acceleration occurs
+accel_max_time = t(accel_max_t);
+
+% Plot acceleration against time
 figure
 hold on
 plot(t, accel_2ndorder,'DisplayName','Acceleration(t)')
@@ -16,9 +28,11 @@ xlabel('Time (s)')
 ylabel('Jumper acceleration (m/s^2)')
 title('Acceleration during jump & Maximum acceleration')
 hold on
-vertline = yline(accel_2ndorder(accel_max_t),'r--');%,'DisplayName',append('accel = ',num2str(accel_2ndorder(accel_max_t)),' m/s^2'))
-horiline = xline(t(accel_max_t),'r--');%,'DisplayName',append('t = ',num2str(t(accel_max_t)), ' secs'))
-plot(t(accel_max_t),accel_2ndorder(accel_max_t),'r*', 'MarkerSize', 10,'DisplayName',append('Max: ',num2str(accel_2ndorder(accel_max_t)),' m/s^2, ',num2str(t(accel_max_t)), ' secs'))
+vertline = yline(accel_2ndorder(accel_max_t),'r--');
+horiline = xline(accel_max_time,'r--');
+plot(accel_max_time,accel_2ndorder(accel_max_t),'r*', 'MarkerSize', 10,'DisplayName',['Max: ' num2str(accel_2ndorder(accel_max_t)) ' m/s^2, ' num2str(accel_max_time) ' secs'])
 vertline.Annotation.LegendInformation.IconDisplayStyle = 'off';
 horiline.Annotation.LegendInformation.IconDisplayStyle = 'off';
 legend;
+
+end
