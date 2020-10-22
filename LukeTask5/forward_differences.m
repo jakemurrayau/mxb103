2b1678126adbedff377a6e31f6d7606f8079f7f6
LukeStockham <71759405+LukeStockham@users.noreply.github.com>
2020-10-22 14:23:51 +1000

    Add files via upload

--- LukeTask5/forward_differences.m
+++ LukeTask5/forward_differences.m
@@ -1,21 +1,21 @@
 function T = forward_differences(Y)
-% T = forward_differences(Y) returns Newton's forward difference table
-% for the function values Y = [y0,y1,...,yn].
+% FORWARD_DIFFERENCES Constructs Newton's forward difference table
+% T = forward_differences(Y) returns Newton's forward difference table for
+% the function values Y = [y0,y1,...,yn].
 % Note that the forward difference table T is stored as:
 % y0
 % y1  Delta y0
 % y2  Delta y1  Delta^2 y0
 % y3  Delta y2  Delta^2 y1  Delta^3 y0
-% etc.
 % The rest of the array T is zero.
 
 n = length(Y);
-T = zeros(n,n);     % create empty FDT
-T(:,1) = Y;         % fill first column
+T = zeros(n,n);     % Create empty FDT
+T(:,1) = Y;         % Fill first column
 
 for j = 2:n
     for i = j:n
-        T(i,j) = T(i,j-1) - T(i-1,j-1); % fill remaiing coloumns
+        T(i,j) = T(i,j-1) - T(i-1,j-1); % Fill remaiing coloumns
     end
 end
 
--- LukeTask5/forward_eval.m
+++ LukeTask5/forward_eval.m
@@ -1,25 +1,20 @@
 function y = forward_eval(X, T, x)
-% Evaluate Newton's forward difference form of the
+% FORWARD_EVAL Evaluates Newton's forward difference form of the 
 % interpolating polynomial.
-% y = forward_eval(X, T, x) returns y = P(x), where P is 
-% the interpolating polynomial constructed using the 
-% abscissas X = [x0,x1,...,xn] and forward difference table T.
+% y = forward_eval(X, T, x) returns y = P(x), where P is the interpolating  
+% polynomial constructed using the abscissas X = [x0,x1,...,xn] and forward
+% difference table T.
 
-[m,n] = size(T);
-if m ~= n
-    error('T must be square.');
-end
-
-% gets the spacing between the abscissas (even spacing is assumed) and
-% uses it to calculate s, from the definition
+% Gets the spacing between the abscissas (even spacing is assumed) and uses
+% it to calculate s, from the definition
 h = X(2)-X(1);
 s = (x - X(1)) / h;
 
-y = zeros(size(x));         % initialise sum
+y = zeros(size(x));         % Initialise sum
 for k = 1:n
-    C = 1/factorial(k-1);   % initialise product of binomial coefficients
+    C = 1/factorial(k-1);   % Initialise product of binomial coefficients
     for i = 0:k-2
        C = C .* (s - i); 
     end
-    y = y + C * T(k,k);     % add next term
+    y = y + C * T(k,k);     % Add next term
 end
