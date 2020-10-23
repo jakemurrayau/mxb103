34c42c41cd53028035e1cd6b8bc4f407ca0ef2e9
Chase Dart <56943448+ChaseDart@users.noreply.github.com>
2020-10-23 11:15:55 +1000

    Add files via upload
    Newest function for task 1 and task 2

--- task1.m
+++ task1.m
@@ -1,14 +1,11 @@
-% The bungee jump company suggests that the standard jump will consist of 10 “bounces”
-% which should take approximately 60 seconds. (Although the jumper will still be in motion at
-% this point, the jump will be considered to be over, and the the jumper will be gently raised
-% all the way back onto the platform above.) Do your model results agree with this timing: 10
-% bounces in around 60 seconds?
+function task1(j,y)
 
-bounces = 0;
+bounces = 0; %Create counter for every bounce within 60 seconds
 
 for i = 2:j
-    if(y(i) > y(i+1) && y(i) > y(i-1))
+    if(y(i) > y(i+1) && y(i) > y(i-1)) %If statement that counts every bounce
         bounces = bounces + 1;
     end
 end
-    fprintf('\n%d bounces',bounces);
+    fprintf('\n%d bounces were completed within 60 seconds',bounces);
+end
