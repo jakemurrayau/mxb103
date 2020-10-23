54ab26bf3503d7bd1de8d01225f04a9b6a3337c9
jakemurrayau <62498904+jakemurrayau@users.noreply.github.com>
2020-10-23 13:48:49 +1000

    Update bungee.m

--- MXB103-Team54-Code/bungee.m
+++ MXB103-Team54-Code/bungee.m
@@ -10,26 +10,26 @@ fprintf('Task 1: %d bounces were completed within 60 seconds \n\n',bounces);
 
 % Task 2
 [maxV, timemaxV] = task2(t, v);
-fprintf('Task 2: Max velocity %.2f m/s at %.2f seconds\n\n', maxV, timemaxV);
+fprintf('Task 2: Max velocity %.4f m/s at %.4f seconds\n\n', maxV, timemaxV);
 
 % Task 3
 [accel_max, accel_max_t] = maxacceleration(v, t, h, n);
-fprintf('Task 3: The max acceleration is %.2f m/s^2 and occurs at %.2f s after the jump\n\n', accel_max*-1, accel_max_t);
+fprintf('Task 3: The max acceleration is %.4f m/s^2 and occurs at %.4f s after the jump\n\n', accel_max, accel_max_t);
 
 % Task 4
 distance = integration_simprule (v, h, n);
-fprintf('Task 4: The total distance travelled during the 60 second jump is %.2f m\n\n', distance);
+fprintf('Task 4: The total distance travelled during the 60 second jump is %.4f m\n\n', distance);
 
 % Task 5
 camera_time = task5_camera(y, t);
-fprintf('Task 5: To take a photo of the jumper, the camera should be set to trigger at %.2f s after the jump\n\n', camera_time)
+fprintf('Task 5: To take a photo of the jumper, the camera should be set to trigger at %.4f s after the jump\n\n', camera_time)
 
 % Task 6
 [dis_from_water_original_jump, dis_from_water_mod_jump, new_k, new_L, max_accel] = modified_bungee(y, t);
-fprintf('Task 6: Originally, at max displacement from the jump point the jumper misses the water by %.2f m.\n', dis_from_water_original_jump)
+fprintf('Task 6: Originally, at max displacement from the jump point the jumper misses the water by %.4f m.\n', dis_from_water_original_jump)
 fprintf('But with the length of the rope modified to %d m, and its spring constant to %d N/m,\n', new_L, new_k)
-fprintf('The jumper now touches the water, reaching %.2f m into it.\n', dis_from_water_mod_jump*-1)
-fprintf('The max acceleration of the jumper also remains just under 2g, with it reaching up to %.2f m/s^2\n\n', max_accel)
+fprintf('The jumper now touches the water, reaching %.4f m into it.\n', dis_from_water_mod_jump*-1)
+fprintf('The max acceleration of the jumper also remains just under 2g, with it reaching up to %.4f m/s^2\n\n', max_accel)
 
 %%
 %Other figures
