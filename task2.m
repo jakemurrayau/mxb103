2dd2fe7dd7e3f04308b8f46a8f1226f2674acd81
jakemurrayau <62498904+jakemurrayau@users.noreply.github.com>
2020-10-23 12:54:19 +1000

    Update task2.m

--- task2.m
+++ task2.m
@@ -1,13 +1,15 @@
-function task2 (t,v)
+function [maxV,timemaxV] = task2(t,v)
 % The company wants to check the maximum speed that will be experienced by jumper to know the "Thrill
 % factor". The graph below describes the velocity over the time of 60 seconds, starting from the point the
 % jumper falls.
 
+[maxV, num] = max(v); 
+timemaxV = t(num);
+figure, plot(t,v,'DisplayName','Velocity(t)')
+hold on
+plot(t(num), maxV, 'r*','MarkerSize',10,'DisplayName',['Max: ' num2str(maxV) ' m/s, ' num2str(timemaxV) ' secs'])
 xlabel('seconds');
 ylabel('velocity');
-[maxV, num] = max(v); 
-fprintf('Task 2:\nMax velocity %.2fm/s at %.2f seconds\n', maxV, t(num));
-plot(t,v)
-hold on
-plot(t(num), maxV, 'r*')
+title('Velocity vs Time')
+legend
 end
